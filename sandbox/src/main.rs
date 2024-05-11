daf68540f7a8e6b2f7159e9e3f8a24ca5e2c7216
Bjorn <rster2002dev@gmail.com>
2024-05-11 14:32:27 +0200

    🚧 Commit sandbox changes

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,28 +1,40 @@
 use std::env::current_dir;
+use std::path::PathBuf;
 use tokio::fs::File;
-use ed_journals::r#async::LiveJournalDirReader;
-use ed_journals::r#async::LiveJournalFileReader;
-use ed_journals::r#async::JournalFileReader;
+use ed_journals::logs::asynchronous::LiveLogDirReader;
+use ed_journals::status::blocking::StatusFileWatcher;
 
 #[tokio::main]
 async fn main() {
-    let dir_path = current_dir()
-        .unwrap()
-        .join("log-dir");
-        // .join("Journal.2024-01-01T154931.01.log");
-    //
-    // let file = File::open()
-    //     .await
-    //     .unwrap();
-
-    // let mut reader = LiveJournalFileReader::create(dir_path.join("Journal.2024-01-01T154931.01.log"))
-    //     .await
-    //     .unwrap();
-    //
-    let mut reader = LiveJournalDirReader::create(dir_path)
+    let watcher = StatusFileWatcher::new(PathBuf::from("./log-dir/Status.json"))
         .unwrap();
 
-    while let Some(entry) = reader.next().await {
-        dbg!(entry);
+    for update in watcher {
+        dbg!(update);
     }
+
+    // let status = ed_journals::status::blocking::read_status_file("./log-dir/Status.json")
+    //     .unwrap();
+    //
+    // dbg!(status);
+
+    // let dir_path = current_dir()
+    //     .unwrap()
+    //     .join("log-dir");
+    //     // .join("Journal.2024-01-01T154931.01.log");
+    // //
+    // // let file = File::open()
+    // //     .await
+    // //     .unwrap();
+    //
+    // // let mut reader = LiveJournalFileReader::create(dir_path.join("Journal.2024-01-01T154931.01.log"))
+    // //     .await
+    // //     .unwrap();
+    // //
+    // let mut reader = LiveLogDirReader::open(dir_path)
+    //     .unwrap();
+    //
+    // while let Some(entry) = reader.next().await {
+    //     dbg!(entry);
+    // }
 }
