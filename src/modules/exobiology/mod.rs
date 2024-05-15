0159063fce23e0212fb755b1c88f9ef334f5c131
Lucas <lucas@somfic.dev>
2024-05-15 19:45:12 +0200

    ✨ Added `get_spawnable_species` function
    This commit adds a new function `check_species_can_spawn` to the `exobiology` module. This function is used to check if a given species can spawn on a planet based on the planet scan event. It iterates over the spawn conditions of each species and returns `true` if all conditions are met.
    
    The function `get_spawnable_species` has also been implemented, which returns a list of species that can spawn on the given planet.

--- src/modules/exobiology/mod.rs
+++ src/modules/exobiology/mod.rs
@@ -1,1 +1,21 @@
+use crate::{
+    logs::content::log_event_content::scan_event::ScanEventPlanet,
+    models::exploration::species::Species,
+};
+use strum::IntoEnumIterator;
+
 pub mod models;
+
+/// Returns a list of species that can spawn on the given planet.
+pub fn get_spawnable_species(planet_scan: &ScanEventPlanet) -> Vec<Species> {
+    Species::iter()
+        .filter(|species| check_species_can_spawn(planet_scan, species))
+        .collect()
+}
+
+fn check_species_can_spawn(planet_scan: &ScanEventPlanet, species: &Species) -> bool {
+    species
+        .spawn_conditions()
+        .iter()
+        .all(|condition| condition.check_for_planet(planet_scan))
+}
