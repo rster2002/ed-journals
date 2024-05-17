294908a704c07f56f8ac162c23dd8d046f498fb8
Bjorn <rster2002dev@gmail.com>
2024-05-17 17:01:25 +0200

    ✨ Add function for auto-detecting journal paths

--- src/modules/journal/mod.rs
+++ src/modules/journal/mod.rs
@@ -4,6 +4,8 @@ pub mod blocking;
 pub mod asynchronous;
 
 mod journal_event;
+mod functions;
 
 pub use journal_event::JournalEvent;
+pub use functions::auto_detect_journal_path::auto_detect_journal_path;
 
