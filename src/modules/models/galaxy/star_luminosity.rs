fe179a52338eb2ee88ebbe155c8bdfaf47210325
Lucas <lucas@somfic.dev>
2024-05-17 12:12:21 +0200

    🧪 Added `test_star_luminosity_comparison` test

--- src/modules/models/galaxy/star_luminosity.rs
+++ src/modules/models/galaxy/star_luminosity.rs
@@ -30,4 +30,36 @@ pub enum StarLuminosity {
     #[serde(alias = "0")]
     Zero = 0,
 }
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_star_luminosity_comparison() {
+        assert!(StarLuminosity::O > StarLuminosity::I);
+        assert!(StarLuminosity::I > StarLuminosity::Ia0);
+        assert!(StarLuminosity::Ia0 > StarLuminosity::Ia);
+        assert!(StarLuminosity::Ia > StarLuminosity::Ib);
+        assert!(StarLuminosity::Ib > StarLuminosity::Iab);
+        assert!(StarLuminosity::Iab > StarLuminosity::II);
+        assert!(StarLuminosity::II > StarLuminosity::IIa);
+        assert!(StarLuminosity::IIa > StarLuminosity::IIab);
+        assert!(StarLuminosity::IIab > StarLuminosity::IIb);
+        assert!(StarLuminosity::IIb > StarLuminosity::III);
+        assert!(StarLuminosity::III > StarLuminosity::IIIa);
+        assert!(StarLuminosity::IIIa > StarLuminosity::IIIab);
+        assert!(StarLuminosity::IIIab > StarLuminosity::IIIb);
+        assert!(StarLuminosity::IIIb > StarLuminosity::IV);
+        assert!(StarLuminosity::IV > StarLuminosity::IVa);
+        assert!(StarLuminosity::IVa > StarLuminosity::IVab);
+        assert!(StarLuminosity::IVab > StarLuminosity::IVb);
+        assert!(StarLuminosity::IVb > StarLuminosity::V);
+        assert!(StarLuminosity::V > StarLuminosity::Va);
+        assert!(StarLuminosity::Va > StarLuminosity::Vab);
+        assert!(StarLuminosity::Vab > StarLuminosity::Vb);
+        assert!(StarLuminosity::Vb > StarLuminosity::Vz);
+        assert!(StarLuminosity::Vz > StarLuminosity::VI);
+        assert!(StarLuminosity::VI > StarLuminosity::VII);
+        assert!(StarLuminosity::VII > StarLuminosity::Zero);
+    }
 }
