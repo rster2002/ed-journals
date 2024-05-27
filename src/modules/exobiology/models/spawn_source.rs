442482a23524ffa73051c916744a2bee6f7402c6
Lucas <lucas@somfic.dev>
2024-05-27 04:39:53 +0200

    ♻️ Add back `is_landable` check
    semver: chore

--- src/modules/exobiology/models/spawn_source.rs
+++ src/modules/exobiology/models/spawn_source.rs
@@ -16,7 +16,7 @@ use crate::galaxy::{
     Atmosphere, AtmosphereDensity, AtmosphereType, Nebula, PlanetClass, PlanetComposition,
     StarClass, StarLuminosity, Volcanism, VolcanismType,
 };
-use crate::logs::scan_event::{ScanEventParent};
+use crate::logs::scan_event::ScanEventParent;
 
 #[derive(Debug)]
 pub struct SpawnSource<'a> {
@@ -34,6 +34,10 @@ impl<'a> SpawnSource<'a> {
 
     /// Checks if the given species can spawn on this spawn source.
     pub fn can_spawn_species(&self, species: &Species) -> bool {
+        if self.target_planet.is_landable == false {
+            return false;
+        }
+
         species
             .spawn_conditions()
             .iter()
@@ -42,6 +46,10 @@ impl<'a> SpawnSource<'a> {
 
     /// Checks if the spawn source satisfies the given condition.
     pub fn satisfies_spawn_condition(&self, condition: &SpawnCondition) -> bool {
+        if self.target_planet.is_landable == false {
+            return false;
+        }
+
         match condition {
             SpawnCondition::MinMeanTemperature(min_temp) => {
                 &self.target_planet.surface_temperature >= min_temp
