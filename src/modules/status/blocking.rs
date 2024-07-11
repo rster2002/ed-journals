f4f97b1003e05da56b62a94213a164511b95ef6b
rster2002 <rster2002dev@gmail.com>
2024-07-11 20:36:12 +0200

    ♻️ Fix and improve status json file

--- src/modules/status/blocking.rs
+++ src/modules/status/blocking.rs
@@ -29,10 +29,10 @@ mod tests {
     use crate::tests::test_root;
 
     #[test]
-    fn status_1_file_is_parsed_correctly() {
+    fn none_status_file_is_parsed_correctly() {
         let file = test_root()
             .join("json")
-            .join("Status1.json");
+            .join("StatusNone.json");
 
         let string_contents = read_to_string(&file).unwrap();
 
@@ -40,5 +40,89 @@ mod tests {
 
         dbg!(&status);
         assert!(status.is_ok());
+        assert!(status.unwrap().contents.is_none());
+    }
+
+    #[test]
+    fn supercruise_status_file_is_parsed_correctly() {
+        let file = test_root()
+            .join("json")
+            .join("StatusSupercruise.json");
+
+        let string_contents = read_to_string(&file).unwrap();
+
+        let status = serde_json::from_str::<Status>(&string_contents);
+
+        dbg!(&status);
+
+        let status = status.unwrap()
+            .contents
+            .unwrap();
+
+        assert!(!status.flags.landed());
+        assert!(!status.flags.landing_gear_down());
+        assert!(status.planet_status.is_none());
+    }
+
+    #[test]
+    fn landed_status_file_is_parsed_correctly() {
+        let file = test_root()
+            .join("json")
+            .join("StatusLanded.json");
+
+        let string_contents = read_to_string(&file).unwrap();
+
+        let status = serde_json::from_str::<Status>(&string_contents);
+
+        dbg!(&status);
+        let status = status.unwrap()
+            .contents
+            .unwrap();
+
+        assert!(!status.flags.in_srv());
+        assert!(status.flags.landed());
+        assert!(status.flags.landing_gear_down());
+        assert!(status.planet_status.is_some());
+        assert!(status.kind.is_ship_status());
+    }
+
+    #[test]
+    fn srv_status_file_is_parsed_correctly() {
+        let file = test_root()
+            .join("json")
+            .join("StatusSRV.json");
+
+        let string_contents = read_to_string(&file).unwrap();
+
+        let status = serde_json::from_str::<Status>(&string_contents);
+
+        dbg!(&status);
+        let status = status.unwrap()
+            .contents
+            .unwrap();
+
+        assert!(status.flags.in_srv());
+        assert!(status.planet_status.is_some());
+        assert!(status.kind.is_ship_status());
+    }
+
+    #[test]
+    fn on_foot_status_file_is_parsed_correctly() {
+        let file = test_root()
+            .join("json")
+            .join("StatusOnFoot.json");
+
+        let string_contents = read_to_string(&file).unwrap();
+
+        let status = serde_json::from_str::<Status>(&string_contents);
+
+        dbg!(&status);
+        let status = status.unwrap()
+            .contents
+            .unwrap();
+
+        assert!(status.flags2.on_foot());
+        assert!(status.planet_status.is_some());
+        assert!(status.kind.is_on_foot_status());
     }
 }
