a156b802efa68aae76d8928d36a2e6c10bb292ec
Bjorn <rster2002dev@gmail.com>
2024-08-10 19:34:46 +0200

    📝 Add some more comments

--- src/modules/logs/content/log_event_content/docking_denied_event.rs
+++ src/modules/logs/content/log_event_content/docking_denied_event.rs
@@ -2,26 +2,49 @@ use serde::{Deserialize, Serialize};
 
 use crate::modules::station::StationType;
 
+/// Fired when the player requested docking permission, but was denied.
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct DockingDeniedEvent {
+    /// The name of the station or settlement the player was denied docking for.
     pub station_name: String,
+
+    /// The kind of station the player wanted to dock at.
     pub station_type: StationType,
 
+    /// The market id for the station.
     #[serde(rename = "MarketID")]
     pub market_id: u64,
+
+    /// The reason why the player was denied docking access.
     pub reason: DockingDeniedReason,
 }
 
+/// The reason why the player was denied docking access.
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase", untagged)]
 pub enum DockingDeniedReason {
+    /// There are currently no landing pads available for the player to land on.
     NoSpace,
+
+    /// There are (currently) no landing pads available for the player's ship size.
     TooLarge,
+
+    /// The player is currently considered hostile to the station.
     Hostile,
+
+    /// The player has outstanding offences (like recently acquired fines)
     Offences,
+
+    /// The player is not within 7.5 km of the station.
     Distance,
+
+    /// The player has an active fighter out.
     ActiveFighter,
+
+    /// The reason was not specified.
     NoReason,
+
+    #[cfg(not(feature = "strict"))]
     Other(String),
 }
