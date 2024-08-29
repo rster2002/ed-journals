54f3459db8bb0601f759dcd1407f9553c7af1844
rster2002 <rster2002dev@gmail.com>
2024-08-29 20:35:39 +0200

    🚧 "Progress"

--- src/modules/logs/content/log_event_content/crime_victim_event.rs
+++ src/modules/logs/content/log_event_content/crime_victim_event.rs
@@ -1,11 +1,19 @@
 use crate::civilization::Crime;
 use serde::{Deserialize, Serialize};
 
+/// Fired when the player
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct CrimeVictimEvent {
+    /// The name of the one that has commited the crime against the player.
     pub offender: String,
+
+    /// The crime committed against the player.
     pub crime_type: Crime,
+
+    /// The fine that was dealt.
     pub fine: Option<u64>,
+
+    /// The bounty that was placed.
     pub bounty: Option<u64>,
 }
--- src/modules/logs/content/log_event_content/datalink_scan_event.rs
+++ src/modules/logs/content/log_event_content/datalink_scan_event.rs
@@ -1,10 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+/// Fired when the player scans a datapoint and extracts a message from it.
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct DatalinkScanEvent {
+    /// Message extracted from the datapoint.
     pub message: String,
 
+    /// The localized message extracted from the datapoint.
     #[serde(rename = "Message_Localised")]
     pub message_localized: Option<String>,
 }
