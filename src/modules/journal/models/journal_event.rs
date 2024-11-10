c36b4944a9d128bff65ad4ae130f93739714ff60
Lucas <lucas@somfic.dev>
2024-11-10 15:17:44 +0100

    🧑‍💻 Derive `Serialize` for `JournalEvent`
    semver: chore

--- src/modules/journal/models/journal_event.rs
+++ src/modules/journal/models/journal_event.rs
@@ -1,6 +1,7 @@
 use crate::journal::JournalEventKind;
+use serde::Serialize;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct JournalEvent {
     /// Indicates whether the event was fired from a change in the journal directory or whether
     /// it's an event from a log file other than the current one.
--- src/modules/journal/models/journal_event_kind.rs
+++ src/modules/journal/models/journal_event_kind.rs
@@ -8,10 +8,11 @@ use crate::nav_route::NavRoute;
 use crate::ship_locker::ShipLocker;
 use crate::shipyard::Shipyard;
 use crate::status::Status;
+use serde::Serialize;
 
 /// This event is fired from the [LiveJournalDirReader] when any change happens in the journal
 /// directory and includes all the possible models that could have been updated.
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Serialize)]
 // The large enum variant is allowed here as this is usually allocated by the reader anyway and
 // adding another box here wouldn't be that useful. Also even though it's large, it's not huge.
 #[allow(clippy::large_enum_variant)]
