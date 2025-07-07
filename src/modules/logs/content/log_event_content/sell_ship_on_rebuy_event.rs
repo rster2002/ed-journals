f15cc5a697619e003796fd10e503bc532e277b9f
Vladimir Kravtsov <vladimir.kravtsov@mindpeak.ai>
2025-07-07 22:16:30 +0200

    Fix SellShipOnRebuyEvent

--- src/modules/logs/content/log_event_content/sell_ship_on_rebuy_event.rs
+++ src/modules/logs/content/log_event_content/sell_ship_on_rebuy_event.rs
@@ -8,7 +8,6 @@ pub struct SellShipOnRebuyEvent {
     pub ship_type: ShipType,
     pub system: String,
 
-    #[serde(rename = "SellShipID")]
     pub sell_ship_id: u64,
     pub ship_price: u64,
 }
