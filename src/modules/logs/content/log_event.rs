e8f346ad09ac2e89aaffbf738801b754a10103d9
CMDR-WDX <83889860+CMDR-WDX@users.noreply.github.com>
2025-08-01 20:39:25 +0200

    style: add comments, resolve clippy issues

--- src/modules/logs/content/log_event.rs
+++ src/modules/logs/content/log_event.rs
@@ -15,54 +15,49 @@ pub struct LogEvent {
 }
 
 #[cfg(feature = "schemars")]
-mod test {
+#[cfg(test)]
+mod integration_test {
     use std::{env::temp_dir, fs::File};
 
     use schemars::JsonSchema;
     use serde::{Deserialize, Serialize};
     use serde_json::{json, Value};
 
-    use crate::logs::{
-        buy_micro_resource_event::BuyMicroResourceEvent, cargo_event::CargoEvent,
-        ship_targeted_event::ShipTargetedEvent, LogEventContent,
-    };
-
     #[derive(Serialize, Deserialize, JsonSchema)]
     struct Noop {}
 
     use super::LogEvent;
 
+    /// Schemars will generate a JSON Schema that contains a oneOf List of element that contain both a $ref and inlined properties.
+    /// This causes code-generators to panic. A work-around is to convert this list of oneOfs into a list of oneOfs that themselves contain a list of anyOfs.
+    /// In this anyOf, we reference the $ref in a single block, and inlined properties in another.
     pub fn fix_oneof_refs(schema: &mut Value) {
         match schema {
             Value::Object(map) => {
-                // Handle oneOf transformation
-                if let Some(one_of) = map.get_mut("oneOf") {
-                    if let Value::Array(items) = one_of {
-                        for i in 0..items.len() {
-                            if let Value::Object(obj) = &items[i] {
-                                if let Some(Value::String(ref_ref)) = obj.get("$ref") {
-                                    // If there are other keys besides $ref
-                                    let other_keys: Vec<_> =
-                                        obj.keys().filter(|k| *k != "$ref").cloned().collect();
+                if let Some(Value::Array(items)) = map.get_mut("oneOf") {
+                    for item in items {
+                        if let Value::Object(obj) = item {
+                            if let Some(Value::String(ref_ref)) = obj.get("$ref") {
+                                // If there are other keys besides $ref
+                                let other_keys: Vec<_> =
+                                    obj.keys().filter(|k| *k != "$ref").cloned().collect();
 
-                                    if !other_keys.is_empty() {
-                                        let mut extras = serde_json::Map::new();
-                                        for key in other_keys {
-                                            if let Some(v) = obj.get(&key) {
-                                                extras.insert(key, v.clone());
-                                            }
+                                if !other_keys.is_empty() {
+                                    let mut extras = serde_json::Map::new();
+                                    for key in other_keys {
+                                        if let Some(v) = obj.get(&key) {
+                                            extras.insert(key, v.clone());
                                         }
-
-                                        // Create allOf structure
-                                        let new_obj = json!({
-                                            "allOf": [
-                                                { "$ref": ref_ref },
-                                                Value::Object(extras)
-                                            ]
-                                        });
-
-                                        items[i] = new_obj;
                                     }
+
+                                    let new_obj = json!({
+                                        "allOf": [
+                                            { "$ref": ref_ref },
+                                            Value::Object(extras)
+                                        ]
+                                    });
+
+                                    *item = new_obj;
                                 }
                             }
                         }
@@ -83,15 +78,17 @@ mod test {
         }
     }
 
+    /// A manual "test" that will output the schema into tmp dir.
     #[test]
-    fn misc() {
-        let mut dir = temp_dir();
+    #[ignore]
+    fn generate_schema() {
+        let dir = temp_dir();
         let mut schema = schemars::schema_for!(LogEvent).to_value();
         fix_oneof_refs(&mut schema);
 
         eprintln!("path: {}", dir.display());
 
-        let file = File::create(dir.join(format!("out.json"))).unwrap();
+        let file = File::create(dir.join("schema.json")).unwrap();
         serde_json::to_writer_pretty(file, &schema).unwrap();
     }
 }
