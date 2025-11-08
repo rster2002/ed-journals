034c2da5b26539839650d389c096e780144134ab
rster2002 <rster2002dev@gmail.com>
2025-11-08 14:31:03 +0100

    🚧 Copy over LogPath from previous branch

--- src/modules/io/mod.rs
+++ src/modules/io/mod.rs
@@ -3,6 +3,8 @@ mod models;
 
 pub use error::LogError;
 pub use models::log_iter::LogIter;
+pub use models::log_file_watcher::LogFileWatcher;
+pub use models::log_path::LogPath;
 
 #[cfg(feature = "asynchronous")]
 pub use models::async_iter::AsyncIter;
--- src/modules/io/models.rs
+++ src/modules/io/models.rs
@@ -1,5 +1,6 @@
+pub mod log_path;
 pub mod log_iter;
+pub mod log_file_watcher;
 
 #[cfg(feature = "asynchronous")]
 pub mod async_iter;
-mod log_file_watcher;
